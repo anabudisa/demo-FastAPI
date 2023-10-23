d83ef78dfa9ba52d243714a9e50cb9d1115a181d
anabudisa <anbudisa@gmail.com>
2023-10-23 18:10:06 +0200

    update app operations to input order model

--- demo_fastapi/model.py
+++ demo_fastapi/model.py
@@ -4,36 +4,39 @@ import re
 
 
 class Order(BaseModel):
-    id: int
-    datestamp: str
-    buyer: str
+    id: int | None = None
+    datestamp: str | None = None
+    buyer: str | None = None
     apples: PositiveInt | None = None
     oranges: PositiveInt | None = None
 
     @field_validator("datestamp")
     @classmethod
-    def check_date_format(cls, ymd: str) -> str:
-        # check if the datetime is in format yyyy/mm/dd and after the date 2000/01/01
-        r = re.compile("\d{4}/\d{2}/\d{2}")
-        assert (
-            len(ymd) == 10
-        ), "Date in wrong format! It should be yyyy/mm/dd (no spaces)."
-        assert r.match(
-            ymd
-        ), "Date in wrong format! It should be yyyy/mm/dd (no spaces)."
-        date_ = datetime(year=int(ymd[:4]), month=int(ymd[5:7]), day=int(ymd[8:10]))
-        assert date_ >= datetime(
-            2000, 1, 1
-        ), "We only track orders after 1 January 2000"
+    def check_date_format(cls, ymd: str | None) -> str | None:
+        if ymd is not None:
+            # check if the datetime is in format yyyy/mm/dd and after the date
+            # 2000/01/01
+            r = re.compile("\d{4}/\d{2}/\d{2}")
+            assert (
+                len(ymd) == 10
+            ), "Date in wrong format! It should be yyyy/mm/dd (no spaces)."
+            assert r.match(
+                ymd
+            ), "Date in wrong format! It should be yyyy/mm/dd (no spaces)."
+            date_ = datetime(year=int(ymd[:4]), month=int(ymd[5:7]), day=int(ymd[8:10]))
+            assert date_ >= datetime(
+                2000, 1, 1
+            ), "We only track orders after 1 January 2000"
         return ymd
 
     @field_validator("buyer")
     @classmethod
-    def check_for_numbers(cls, x: str) -> str:
-        # check if the string contains any numbers
-        assert not any(
-            char.isdigit() for char in x
-        ), "Buyer's name cannot contain numbers!"
+    def check_for_numbers(cls, x: str | None) -> str | None:
+        if x is not None:
+            # check if the string contains any numbers
+            assert not any(
+                char.isdigit() for char in x
+            ), "Buyer's name cannot contain numbers!"
         return x
 
     @model_validator(mode="after")
--- demo_fastapi/sales.py
+++ demo_fastapi/sales.py
@@ -1,10 +1,8 @@
 from datetime import datetime
-from pydantic import PositiveInt
 from fastapi import FastAPI, HTTPException
 from .model import Order
 from .connection_manager import get_db
 from contextlib import asynccontextmanager
-import re
 import pyodbc
 import random
 
@@ -23,71 +21,41 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/orders/")
-def create_order(
-    datestamp: str,
-    buyer: str,
-    apples: PositiveInt = None,
-    oranges: PositiveInt = None,
-):
+def create_order(order: Order):
     """
-    Create an order on date "datestamp", from buyer "buyer" that is buying "sale";
+    Create an order on date "datestamp", from buyer "buyer" that is buying
+    "apples" and "oranges";
     """
     cnxn = app.state.connection_manager.connection
-    # check if any apples or oranges are ordered
-    if apples is None and oranges is None:
+
+    # check if date is provided
+    if order.datestamp is None:
         raise HTTPException(
             status_code=422,
-            detail="No sale has been made! Order at least one apple or orange.",
+            detail="Please provide date of the order in format yyyy/mm/dd",
         )
-
-    # check if the name contains any numbers
-    if any(char.isdigit() for char in buyer):
-        raise HTTPException(
-            status_code=422, detail="Buyer's name cannot contain numbers!"
-        )
-
-    # check date format and if it's after 1 January 2000
-    r = re.compile(r"\d{4}/\d{2}/\d{2}")
-    if len(datestamp) != 10:
+    # check if buyer's name is provided
+    if order.buyer is None:
         raise HTTPException(
             status_code=422,
-            detail="Date in wrong format! It should be yyyy/mm/dd (no spaces). "
-            + "Perhaps check for typos?",
-        )
-    if not r.match(datestamp):
-        raise HTTPException(
-            status_code=422,
-            detail="Date in wrong format! It should be yyyy/mm/dd (no spaces).",
-        )
-
-    try:
-        date_ = datetime.strptime(datestamp, "%Y/%m/%d")
-    except ValueError as err:
-        raise HTTPException(status_code=422, detail=str(err))
-
-    if date_ < datetime(2000, 1, 1):
-        raise HTTPException(
-            status_code=422,
-            detail="We only track orders after 1 January 2000. "
-            + "Please enter only valid orders.",
+            detail="Please enter buyer's name "
+            "(max 50 characters, without numbers in the name).",
         )
 
     # if everything ok, make an order
     order_id = random.randrange(10**8)  # 12345  # uuid1().int
-    order = Order(
-        id=order_id, datestamp=datestamp, buyer=buyer, apples=apples, oranges=oranges
-    )
+    order.id = order_id
 
     # save to the database
     cursor = cnxn.cursor()
     try:
         cursor.execute(
             "INSERT INTO ShoppingList VALUES (?,?,?,?,?)",
-            datestamp,
-            buyer,
-            apples,
-            oranges,
-            order_id,
+            order.datestamp,
+            order.buyer,
+            order.apples,
+            order.oranges,
+            order.id,
         )
         cursor.commit()
     except pyodbc.DatabaseError as err:
@@ -100,23 +68,17 @@ def create_order(
 
 
 @app.put("/orders/")
-def update_order(
-    order_id: int,
-    datestamp: str = None,
-    buyer: str = None,
-    apples: PositiveInt = None,
-    oranges: PositiveInt = None,
-):
+def update_order(order: Order):
     """update order with ID = order_id"""
     cnxn = app.state.connection_manager.connection
     cursor = cnxn.cursor()
-    cursor.execute("SELECT * FROM ShoppingList WHERE id = ?", order_id)
+    cursor.execute("SELECT * FROM ShoppingList WHERE id = ?", order.id)
     row = cursor.fetchone()
 
-    datestamp = datestamp if datestamp is not None else row.datestamp
-    buyer = buyer if buyer is not None else row.buyer
-    apples = apples if apples is not None else row.apples
-    oranges = oranges if oranges is not None else row.oranges
+    datestamp = order.datestamp or row.datestamp
+    buyer = order.buyer or row.buyer
+    apples = order.apples or row.apples
+    oranges = order.oranges or row.oranges
 
     try:
         cursor.execute(
@@ -126,7 +88,7 @@ def update_order(
             buyer,
             apples,
             oranges,
-            order_id,
+            order.id,
         )
         cursor.commit()
     except pyodbc.DatabaseError as err:
@@ -135,9 +97,21 @@ def update_order(
             detail="Error updating database! Recheck your entries.\n" + str(err),
         )
 
-    # order = Order(datestamp=datestamp, buyer=buyer, apples=apples, oranges=oranges)
+    # if the change passed, log the last version in the log table
+    date_changed = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    cursor.execute(
+        "INSERT INTO ShoppingListLog VALUES (?,?,?,?,?,?)",
+        row.datestamp,
+        row.buyer,
+        row.apples,
+        row.oranges,
+        row.id,
+        date_changed,
+    )
+    cursor.commit()
+
     return Order(
-        id=order_id, datestamp=datestamp, buyer=buyer, apples=apples, oranges=oranges
+        id=order.id, datestamp=datestamp, buyer=buyer, apples=apples, oranges=oranges
     )
 
 
